f18254cb248312fd2d6640d720d8f9b202161a04
Nathan Blanchard <nathanblanchard@Nathans-MacBook-Pro.local>
2019-10-30 17:03:55 -0400

    day

--- src/components/SelectedDay/SelectedDay.js
+++ src/components/SelectedDay/SelectedDay.js
@@ -1,46 +1,50 @@
-import React from 'react';
-import Context from '../../Context'
+import React from "react";
+import Context from "../../Context";
 import { NavLink } from "react-router-dom";
 // import ApiService from '../../services/api-service';
 
-import './SelectedDay.css'
-import { getDay } from 'date-fns/esm';
-
+import "./SelectedDay.css";
+// import { getDay } from "date-fns/esm";
 
 export default class SelectedDay extends React.Component {
   state = {
-    selectedDay: null,
+    selectedDay: null
+  };
+
+  static contextType = Context;
+
+  renderPoster(day) {
+    const foundDay = this.context.bigObj;
+    const seekDay = "oct" + day.toString();
+    console.log(day)
+    //  Object.keys(this.context.bigObj)
+    //  .filter(key => this.context.bigObj[key] === seekDay)
+   
+    console.log(foundDay["oct" + day.toString()]);
   }
 
-  static contextType = Context
-
-
-
- renderPoster(day) {
-   const seekDay = 'oct'+day
-   const foundDay = 
-   Object.keys(this.context.bigObj)
-   .filter(key => this.context.bigObj[key] === seekDay)
-
-   console.log(foundDay)  
-}
- 
   render() {
-    const { id } = this.context.bigObj
+    // const { id } = this.context.bigObj;
     return (
       <>
-      <h2 className="date-banner">
-        October {this.props.day}
-      </h2>
-      <section className="dinner-and-movie">
-        <NavLink to={`/list`} className="add-move">
-          {this.renderPoster(this.props.day)}
-          <img className="selected-movie-image" src='https://dummyimage.com/200x300/000/fff.png&text=Add+Movie' alt="add movie"/>
-        </NavLink>
+        <h2 className="date-banner">October {this.props.day}</h2>
+        <section className="dinner-and-movie">
+          <NavLink to={`/list`} className="add-move">
+            {this.renderPoster(this.props.day)}
+            <img
+              className="selected-movie-image"
+              src="https://dummyimage.com/200x300/000/fff.png&text=Add+Movie"
+              alt="add movie"
+            />
+          </NavLink>
 
-        <img className="add-meal" src='https://dummyimage.com/200x300/000000/fff.png&text=Add+Meal' alt="add meal" />
-      </section>
+          <img
+            className="add-meal"
+            src="https://dummyimage.com/200x300/000000/fff.png&text=Add+Meal"
+            alt="add meal"
+          />
+        </section>
       </>
-    )
+    );
   }
 }
