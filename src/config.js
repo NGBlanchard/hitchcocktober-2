9ecd26dc0b8bcb4400038e4df27117e422364e5f
Nathan Blanchard <nathanblanchard@Nathans-MacBook-Pro-3.local>
2020-10-07 15:35:04 -0400

    api endpoint correction

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:9090',
-  // API_ENDPOINT: 'https://shrouded-sierra-65437.herokuapp.com',
+  // API_ENDPOINT: 'http://localhost:9090',
+  API_ENDPOINT: 'https://shrouded-sierra-65437.herokuapp.com',
   TOKEN_KEY: 'hitchcocktober-auth-token',
   CURRENT_USER: 'user',
   MEMBER_SINCE: "date",
